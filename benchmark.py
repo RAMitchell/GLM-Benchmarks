56cc06df24f5b7a67edcfe53ce67e8b717f01f29
Rory Mitchell <r.a.mitchell.nz@gmail.com>
2018-02-28 16:37:42 +1300

    Refactor

--- benchmark.py
+++ benchmark.py
@@ -1,72 +1,13 @@
+import datasets
 import glmnet
-import gzip
 import numpy as np
-import os
 import pandas as pd
-import shutil
-import tarfile
 import time
-import urllib
 import xgboost as xgb
-import zipfile
-from sklearn import datasets, metrics, linear_model, preprocessing
-from sklearn.externals.joblib import Memory
-
-mem = Memory("./mycache")
+from sklearn import metrics, linear_model, preprocessing
 
 num_rounds = 1000
 early_stopping_rounds = 5
-
-
-@mem.cache
-def get_higgs():
-    url = 'https://archive.ics.uci.edu/ml/machine-learning-databases/00280/HIGGS.csv.gz'
-    filename = 'HIGGS.csv'
-    if not os.path.isfile(filename):
-        urllib.urlretrieve(url, filename + '.gz')
-        with gzip.open(filename + '.gz', 'rb') as f_in:
-            with open(filename, 'wb') as f_out:
-                shutil.copyfileobj(f_in, f_out)
-    higgs = pd.read_csv(filename)
-    X = higgs.iloc[:, 1:].values
-    y = higgs.iloc[:, 0].values
-
-    return X, y
-
-
-@mem.cache
-def get_year():
-    url = 'https://archive.ics.uci.edu/ml/machine-learning-databases/00203/YearPredictionMSD.txt.zip'
-    filename = 'YearPredictionMSD.txt'
-    if not os.path.isfile(filename):
-        urllib.urlretrieve(url, filename + '.zip')
-        zip_ref = zipfile.ZipFile(filename + '.zip', 'r')
-        zip_ref.extractall()
-        zip_ref.close()
-
-    year = pd.read_csv('YearPredictionMSD.txt', header=None)
-    X = year.iloc[:, 1:].values
-    y = year.iloc[:, 0].values
-
-    return X, y
-
-
-@mem.cache
-def get_url():
-    url = 'https://archive.ics.uci.edu/ml/machine-learning-databases/url/url_svmlight.tar.gz'
-    filename = 'url_svmlight.tar.gz'
-    if not os.path.isfile(filename):
-        urllib.urlretrieve(url)
-    tar = tarfile.open(filename, "r:gz")
-    tar.extractall()
-    tar.close()
-
-    data = datasets.load_svmlight_file('url_svmlight/Day0.svm')
-    X = data[0]
-
-    y = data[1]
-    y[y < 0.0] = 0.0
-    return X, y
 
 
 def is_float(s):
@@ -80,7 +21,8 @@ def is_float(s):
 def xgb_get_weights(bst):
     return [float(s) for s in bst.get_dump()[0].split() if is_float(s)][1:]
 
-def run_xgboost_regression(df,X, y, param, dataset,reg_alpha,reg_lambda):
+
+def run_xgboost_regression(df, X, y, param, dataset, reg_alpha, reg_lambda, metric, density):
     tmp = time.time()
     dtrain = xgb.DMatrix(X, label=y, nthread=-1)
     bst = xgb.train(param, dtrain, num_rounds, [(dtrain, 'train')], early_stopping_rounds=early_stopping_rounds,
@@ -89,18 +31,20 @@ def run_xgboost_regression(df,X, y, param, dataset,reg_alpha,reg_lambda):
     xgb_score = np.sqrt(metrics.mean_squared_error(y, bst.predict(dtrain)))
     xgb_zero = X.shape[1] - np.count_nonzero(xgb_get_weights(bst))
     xgb_iterations = bst.best_iteration + early_stopping_rounds
-    df.loc[len(df)] = [dataset, X.shape[1], X.shape[0], 'Regression', reg_alpha, reg_lambda, 'XGBoost', metric,
+    df.loc[len(df)] = [dataset, X.shape[1], X.shape[0], density, 'Regression', reg_alpha, reg_lambda, 'XGBoost', metric,
                        xgb_score,
                        xgb_time, xgb_iterations, xgb_zero]
+
 
 def run_regression(df, dataset, X, y, reg_alpha, reg_lambda, standardize=True):
     if standardize:
         X = preprocessing.StandardScaler().fit_transform(X)
         y = preprocessing.scale(y)
     metric = 'RMSE'
+    density = np.count_nonzero(X) / float(X.shape[0] * X.shape[1])
     param = {'booster': 'gblinear', 'updater': 'coord_descent', 'reg_alpha': reg_alpha, 'reg_lambda': reg_lambda,
              'debug_verbose': 1}
-    run_xgboost_regression(df,X, y, param, dataset,reg_alpha,reg_lambda)
+    run_xgboost_regression(df, X, y, param, dataset, reg_alpha, reg_lambda, metric, density)
 
     tmp = time.time()
     enet = linear_model.ElasticNet(alpha=reg_alpha + reg_lambda, l1_ratio=reg_alpha / (reg_alpha + reg_lambda))
@@ -108,7 +52,7 @@ def run_regression(df, dataset, X, y, reg_alpha, reg_lambda, standardize=True):
     enet_time = time.time() - tmp
     enet_score = np.sqrt(metrics.mean_squared_error(y, enet.predict(X)))
     enet_zero = X.shape[1] - np.count_nonzero(enet.coef_)
-    df.loc[len(df)] = [dataset, X.shape[1], X.shape[0], 'Regression', reg_alpha, reg_lambda, 'Sklearn', metric,
+    df.loc[len(df)] = [dataset, X.shape[1], X.shape[0], density, 'Regression', reg_alpha, reg_lambda, 'Sklearn', metric,
                        enet_score,
                        enet_time, enet.n_iter_, enet_zero]
 
@@ -118,13 +62,12 @@ def run_regression(df, dataset, X, y, reg_alpha, reg_lambda, standardize=True):
     glmnet_time = time.time() - tmp
     glmnet_score = np.sqrt(metrics.mean_squared_error(y, glm.predict(X, lamb=[reg_alpha + reg_lambda])))
     glmnet_zero = X.shape[1] - np.count_nonzero(glm.coef_)
-    df.loc[len(df)] = [dataset, X.shape[1], X.shape[0], 'Regression', reg_alpha, reg_lambda, 'Glmnet', metric,
+    df.loc[len(df)] = [dataset, X.shape[1], X.shape[0], density, 'Regression', reg_alpha, reg_lambda, 'Glmnet', metric,
                        glmnet_score,
                        glmnet_time, '-', glmnet_zero]
-    print(df.to_string())
 
 
-def run_xgboost_classification(df,X, y, param, dataset,reg_alpha,reg_lambda, metric):
+def run_xgboost_classification(df, X, y, param, dataset, reg_alpha, reg_lambda, metric, density):
     dtrain = xgb.DMatrix(X, label=y, nthread=-1)
     tmp = time.time()
     bst = xgb.train(param, dtrain, num_rounds, [(dtrain, 'train')], early_stopping_rounds=early_stopping_rounds,
@@ -132,23 +75,27 @@ def run_xgboost_classification(df,X, y, param, dataset,reg_alpha,reg_lambda, met
     xgb_time = time.time() - tmp
     xgb_score = metrics.accuracy_score(y, np.round(bst.predict(dtrain)))
     xgb_zero = X.shape[1] - np.count_nonzero(xgb_get_weights(bst))
-    df.loc[len(df)] = [dataset, X.shape[1], X.shape[0], 'Classification', reg_alpha, reg_lambda, 'XGBoost', metric,
+    df.loc[len(df)] = [dataset, X.shape[1], X.shape[0], density, 'Classification', reg_alpha, reg_lambda, 'XGBoost',
+                       metric,
                        xgb_score,
                        xgb_time, bst.best_iteration + early_stopping_rounds, xgb_zero]
 
-def run_sklearn_classification(df,X, y, algo ,dataset,reg_alpha,reg_lambda, metric):
+
+def run_sklearn_classification(df, X, y, algo, dataset, reg_alpha, reg_lambda, metric, density):
     tmp = time.time()
     C = 1.0 / (reg_lambda)
-    sklearn = linear_model.LogisticRegression(C=C, solver = algo)
+    sklearn = linear_model.LogisticRegression(C=C, solver=algo)
     sklearn.fit(X, y)
     sklearn_time = time.time() - tmp
     sklearn_score = metrics.accuracy_score(y, sklearn.predict(X))
     sklearn_zero = X.shape[1] - np.count_nonzero(sklearn.coef_)
-    df.loc[len(df)] = [dataset, X.shape[1], X.shape[0], 'Classification', reg_alpha, reg_lambda, 'Sklearn ({})'.format(algo),
+    df.loc[len(df)] = [dataset, X.shape[1], X.shape[0], density, 'Classification', reg_alpha, reg_lambda,
+                       'Sklearn ({})'.format(algo),
                        metric, sklearn_score,
                        sklearn_time, sklearn.n_iter_, sklearn_zero]
 
-def run_glmnet_classification(df,X, y, dataset,reg_alpha,reg_lambda, metric):
+
+def run_glmnet_classification(df, X, y, dataset, reg_alpha, reg_lambda, metric, density):
     tmp = time.time()
     glm = glmnet.LogitNet(alpha=reg_alpha / (reg_alpha + reg_lambda), lambda_path=[reg_alpha + reg_lambda])
     glm.fit(X, y)
@@ -156,44 +103,44 @@ def run_glmnet_classification(df,X, y, dataset,reg_alpha,reg_lambda, metric):
     glmnet_score = metrics.accuracy_score(y, glm.predict(X))
     glmnet_zero = X.shape[1] - np.count_nonzero(glm.coef_)
 
-    df.loc[len(df)] = [dataset, X.shape[1], X.shape[0], 'Classification', reg_alpha, reg_lambda, 'Glmnet', metric,
+    df.loc[len(df)] = [dataset, X.shape[1], X.shape[0], density, 'Classification', reg_alpha, reg_lambda, 'Glmnet',
+                       metric,
                        glmnet_score,
                        glmnet_time, '-', glmnet_zero]
 
-def run_classification(df, dataset, X, y, reg_alpha, reg_lambda, standardize=True, solvers = None):
+
+def run_classification(df, dataset, X, y, reg_alpha, reg_lambda, standardize=True, solvers=None):
     if solvers is None:
-        solvers = ['XGBoost','Sklearn (liblinear)','Sklearn (sag)','Sklearn (lbfgs)','Glmnet']
+        solvers = ['XGBoost', 'Sklearn (liblinear)', 'Sklearn (sag)', 'Sklearn (lbfgs)', 'Glmnet']
     if standardize:
         X = preprocessing.StandardScaler().fit_transform(X)
     metric = 'Accuracy score'
+    density = np.count_nonzero(X) / float(X.shape[0] * X.shape[1])
     param = {'booster': 'gblinear', 'objective': 'binary:logistic', 'updater': 'coord_descent', 'reg_alpha': reg_alpha,
              'reg_lambda': reg_lambda, 'debug_verbose': 1}
     if 'XGBoost' in solvers:
-        run_xgboost_classification(df,X,y,param,dataset,reg_alpha,reg_lambda, metric)
+        run_xgboost_classification(df, X, y, param, dataset, reg_alpha, reg_lambda, metric, density)
     if 'Sklearn (liblinear)' in solvers:
-        run_sklearn_classification(df,X,y,'liblinear',dataset,reg_alpha,reg_lambda, metric)
+        run_sklearn_classification(df, X, y, 'liblinear', dataset, reg_alpha, reg_lambda, metric, density)
     if 'Sklearn (sag)' in solvers:
-        run_sklearn_classification(df,X,y,'sag',dataset,reg_alpha,reg_lambda, metric)
+        run_sklearn_classification(df, X, y, 'sag', dataset, reg_alpha, reg_lambda, metric, density)
     if 'Sklearn (lbfgs)' in solvers:
-        run_sklearn_classification(df,X,y,'lbfgs',dataset,reg_alpha,reg_lambda, metric)
+        run_sklearn_classification(df, X, y, 'lbfgs', dataset, reg_alpha, reg_lambda, metric, density)
     if 'Glmnet' in solvers:
-        run_glmnet_classification(df,X,y,dataset,reg_alpha,reg_lambda, metric)
+        run_glmnet_classification(df, X, y, dataset, reg_alpha, reg_lambda, metric, density)
 
 
-
-df = pd.DataFrame([], columns=['Dataset', 'Num features', 'Num rows', 'Type', 'L1 penalty', 'L2 penalty', 'Solver'
+df = pd.DataFrame([], columns=['Dataset', 'Num features', 'Num rows', 'Density', 'Type', 'L1 penalty', 'L2 penalty',
+                               'Solver'
     , 'Metric', 'Accuracy', 'Time',
                                'Iterations', 'Zero Coefficients'])
-'''
-X, y = get_year()
+X, y = datasets.get_year()
 run_regression(df, 'YearPredictionMSD', X, y, 0.05, 0.1)
-X,y = get_higgs()
+X, y = datasets.get_higgs()
 run_classification(df, 'Higgs', X, y, 0.0, 0.1)
-'''
-X, y = get_url()
-X = X[:,0:1000000]
-solvers = ['XGBoost','Sklearn (liblinear)','Sklearn (sag)','Sklearn (lbfgs)']
-#solvers = ['XGBoost']
-run_classification(df, 'URL Reputation', X, y, 0.0, 0.000001, False,solvers)
+X, y = datasets.get_url()
+solvers = ['XGBoost', 'Sklearn (liblinear)', 'Sklearn (sag)', 'Sklearn (lbfgs)']
+# solvers = ['XGBoost']
+run_classification(df, 'URL Reputation', X, y, 0.0, 0.000001, False, solvers)
 print(df.to_string())
 df.to_csv('result.csv')
---
+++ datasets.py
@@ -0,0 +1,70 @@
+import shutil
+import urllib
+import tarfile
+import gzip
+import numpy as np
+import zipfile
+import os
+import pandas as pd
+from scipy.sparse import vstack
+from sklearn.externals.joblib import Memory
+from sklearn import datasets
+
+if sys.version_info[0] >= 3:
+    from urllib.request import urlretrieve
+else:
+    from urllib import urlretrieve
+
+mem = Memory("./mycache")
+
+@mem.cache
+def get_higgs():
+    url = 'https://archive.ics.uci.edu/ml/machine-learning-databases/00280/HIGGS.csv.gz'
+    filename = 'HIGGS.csv'
+    if not os.path.isfile(filename):
+        urlretrieve(url, filename + '.gz')
+        with gzip.open(filename + '.gz', 'rb') as f_in:
+            with open(filename, 'wb') as f_out:
+                shutil.copyfileobj(f_in, f_out)
+    higgs = pd.read_csv(filename)
+    X = higgs.iloc[:, 1:].values
+    y = higgs.iloc[:, 0].values
+
+    return X, y
+
+
+@mem.cache
+def get_year():
+    url = 'https://archive.ics.uci.edu/ml/machine-learning-databases/00203/YearPredictionMSD.txt.zip'
+    filename = 'YearPredictionMSD.txt'
+    if not os.path.isfile(filename):
+        urllib.urlretrieve(url, filename + '.zip')
+        zip_ref = zipfile.ZipFile(filename + '.zip', 'r')
+        zip_ref.extractall()
+        zip_ref.close()
+
+    year = pd.read_csv('YearPredictionMSD.txt', header=None)
+    X = year.iloc[:, 1:].values
+    y = year.iloc[:, 0].values
+
+    return X, y
+
+@mem.cache
+def get_url():
+    url = 'https://archive.ics.uci.edu/ml/machine-learning-databases/url/url_svmlight.tar.gz'
+    filename = 'url_svmlight.tar.gz'
+    if not os.path.isfile(filename):
+        urllib.urlretrieve(url)
+        tar = tarfile.open(filename, "r:gz")
+        tar.extractall()
+        tar.close()
+
+    num_files = 120
+    files = ['url_svmlight/Day{}.svm'.format(day) for day in range(num_files)]
+    data = datasets.load_svmlight_files(files)
+    X = vstack(data[::2])
+    y = np.concatenate(data[1::2])
+
+    y[y < 0.0] = 0.0
+    return X, y
+
